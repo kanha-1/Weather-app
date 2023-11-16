9d754479aad182045e8531c15c1f9cb8b8b3fb0f
kanhu <Kahnu.Sahoo@tecnotree.com>
2023-11-16 18:41:40 +0530

    code refactor

--- js/index.js
+++ js/index.js
@@ -18,12 +18,12 @@ function enterPressed(event) {
 
 function findWeatherDetails() {
   if (searchInput.value === "") {
-  
-  }else {
-    let searchLink = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid="+appKey;
-   httpRequestAsync(searchLink, theResponse);
+
+  } else {
+    let searchLink = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid=" + appKey;
+    httpRequestAsync(searchLink, theResponse);
   }
- }
+}
 
 function theResponse(response) {
   let jsonObject = JSON.parse(response);
@@ -33,14 +33,13 @@ function theResponse(response) {
   humidity.innerHTML = jsonObject.main.humidity + "%";
 }
 
-function httpRequestAsync(url, callback)
-{
+function httpRequestAsync(url, callback) {
   console.log("hello");
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = () => { 
-        if (httpRequest.readyState == 4 && httpRequest.status == 200)
-            callback(httpRequest.responseText);
-    }
-    httpRequest.open("GET", url, true); // true for asynchronous 
-    httpRequest.send();
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = () => {
+    if (httpRequest.readyState == 4 && httpRequest.status == 200)
+      callback(httpRequest.responseText);
+  }
+  httpRequest.open("GET", url, true); // true for asynchronous 
+  httpRequest.send();
 }
